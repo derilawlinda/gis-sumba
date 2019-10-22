6fe4ff9dcd174182ee73432462db6b4b49fed1ea
Deril Alwinda <deril@yahoo.com>
2019-10-22 22:55:16 +0700

    change iframe

--- src/routes/peta/index.js
+++ src/routes/peta/index.js
@@ -4,8 +4,7 @@ class Peta extends Component {
     render() {
         return (
             <div>
-                <iframe width="100%" height="520" frameborder="0" src="http://demo.zenit.id/user/dev/builder/96dc8c42-231d-4ca5-95ef-8a21b23d284a/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-
+                <iframe title="Peta" width="100%" height="520" frameborder="0" src="http://demo.zenit.id/user/dev/builder/651f4627-bb04-45cf-a4eb-66a316e8b730/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
             </div>
         );
     }
