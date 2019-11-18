a0db29ab0eb36c0bd7326bf243874f1a9e523c7d
Deril Alwinda <deril@yahoo.com>
2019-11-18 17:51:34 +0700

    container height

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ ReactDOM.render(<Router>
                 <Header />
             </Row>
             <Row>
-                <div style={{ width: "100%", height: "70vh", overflow: 'auto', marginTop:"2vh" }}>
+                <div style={{ width: "100%", height: "67vh", overflow: 'auto', marginTop:"3vh" }}>
                     <Switch>
                         <Route exact path="/" component={Peta} />
                         <Route path="/ringkasan" component={Ringkasan} />
